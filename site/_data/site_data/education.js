205f1cafe652654631b888a37b034682af5907b2
liaporto <moonkoala23@gmail.com>
2021-02-24 17:04:28 -0300

    change education entry description

--- site/_data/site_data/education.js
+++ site/_data/site_data/education.js
@@ -2,9 +2,9 @@ module.exports = {
   "en-us": [
     {
       name: "Federal University of Rio de Janeiro",
-      title: "B.A in Communications - Marketing & Advertising",
-      info: "Bachelor Degree in Marketing & Advertising, currently on third semester.",
-      gpa: "GPA: " + ((9.2 * 4) / 10).toFixed(2),
+      title: "B.A in Communications",
+      info: "Bachelor Degree in Communications, currently on third semester.",
+      gpa: "GPA: " + ((9.3 * 4) / 10).toFixed(2),
       period: "Mar 2019 - Atualmente",
     },
     {
@@ -17,8 +17,8 @@ module.exports = {
     {
       name: "Universidade Federal do Rio de Janeiro",
       title: "Bacharelado em Comunicação Social",
-      info: "Bacharelado em Comunicação Social com habilitação em Publicidade e propaganda. Atualmente no 3º período.",
-      gpa: "C.R. 9.2",
+      info: "Bacharelado em Comunicação Social. Atualmente no 3º período.",
+      gpa: "C.R.A 9.3",
       period: "Mar 2019 - Atualmente",
     },
     {
