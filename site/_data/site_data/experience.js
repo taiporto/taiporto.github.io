e314199fc3c9166dd5c8161c2bfcf83d7a4bbd5d
liaporto <moonkoala23@gmail.com>
2021-05-16 13:06:32 -0300

    Increase margin between work experience paragraphs

--- site/_data/site_data/experience.js
+++ site/_data/site_data/experience.js
@@ -38,7 +38,7 @@ module.exports = {
       title: "Estagiária de Experiência do cliente",
       place: "Naturgy Brasil",
       description:
-        "- Design e implementação do menu de navegação do portal de auto atendimento Minha Naturgy;\n- Atualizações de layout e criação de funcionalidades para a central de ajuda interna e para o Minha Naturgy;\n- Redação de texto e mudanças de layout visando melhoria de SEO e usabilidade;\n- Análise de métricas de uso com Google Analytics e Google Data Studio;\n- Desenvolvimento, aplicação e análise de resultados de pesquisas de satisfação.",
+        "- Design e implementação do menu de navegação do portal de auto atendimento Minha Naturgy;\n- Atualizações de layout e criação de funcionalidades para a central de ajuda interna e para o Minha Naturgy;\n- Redação de texto e mudanças de layout visando melhorias de SEO e usabilidade;\n- Análise de métricas de uso com Google Analytics e Google Data Studio;\n- Desenvolvimento, aplicação e análise de resultados de pesquisas de satisfação.",
       period: "Jan 2020 - Atualmente",
       media: [
         {
