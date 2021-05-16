b8dc985eac582dc4bba16ccaba19752388eec366
liaporto <moonkoala23@gmail.com>
2021-05-15 22:26:58 -0300

    Add new project and change projects data structure

--- site/_data/site_data/projects.js
+++ site/_data/site_data/projects.js
@@ -1,68 +1,101 @@
 const projectsImgsPath = "projects/";
-const calculaecoImgPath = `${projectsImgsPath}calcula-eco.png`;
-const bandejaobotImgPath = `${projectsImgsPath}bandejaobot.png`;
 
-const calculaecoLinks = [
-  {
-    type: "Github",
-    url: 'https://github.com/liaporto/calcula-eco',
-    icon: "fab fa-github"
-  },
-  {
-    type: "Site/Perfil",
-    url:'http://calcula-eco.herokuapp.com/',
-    icon: "fas fa-desktop"
-  },
-]
+const imgPaths = {
+  calculaeco: `${projectsImgsPath}calcula-eco.png`,
+  bandejaoBot: `${projectsImgsPath}bandejaobot.png`,
+  dailyPokemon: `${projectsImgsPath}dailyPokemon.jpg`
+}
 
-const bandejaobotLinks = [
-  {
-    type: "Github",
-    url: 'https://github.com/liaporto/bandejaobot-ufrj',
-    icon: "fab fa-github"
-  },
-  {
-    type: "Site/Perfil",
-    url:'https://twitter.com/bandejaobotufrj',
-    icon: "fas fa-desktop"
-  },
-]
+const links = {
+  calculaecoLinks: [
+    {
+      type: "Github",
+      url: 'https://github.com/liaporto/calcula-eco',
+      icon: "fab fa-github"
+    },
+    {
+      type: "Site/Perfil",
+      url:'http://calcula-eco.herokuapp.com/',
+      icon: "fas fa-desktop"
+    },  
+  ],
+  bandejaobotLinks: [
+    {
+      type: "Github",
+      url: 'https://github.com/liaporto/bandejaobot-ufrj',
+      icon: "fab fa-github"
+    },
+    {
+      type: "Site/Perfil",
+      url:'https://twitter.com/bandejaobotufrj',
+      icon: "fas fa-desktop"
+    },
+  ],
+  dailyPokemonLinks: [
+    {
+      type: "Github",
+      url: 'https://github.com/liaporto/daily-pokemon',
+      icon: "fab fa-github"
+    },
+    {
+      type: "Site/Perfil",
+      url:'https://liaporto.github.io/daily-pokemon',
+      icon: "fas fa-desktop"
+    },
+  ]
+}
 
 module.exports = {
   "en-us": [
     {
       id: "calculaeco_en",
-      img: calculaecoImgPath,
+      img: imgPaths.calculaeco,
       name: "CalculaECO",
-      links: calculaecoLinks,
+      links: links.calculaecoLinks,
       short_description:
-        "CalculaECO is a GPA calculation tool build specifically for UFRJ's Communication majors.",
+        "CalculaECO is a GPA calculation tool built specifically for UFRJ's Communication majors.",
     },
     {
       id: "bandejaobot_en",
-      img: bandejaobotImgPath,
+      img: imgPaths.bandejaoBot,
       name: "BandejãoBot UFRJ",
-      links: bandejaobotLinks,
+      links: links.bandejaobotLinks,
       short_description:
         "BandejãoBot UFRJ is a Twitter bot and it posts UFRJ's cafeteria menu daily at 9AM.",
+    },
+    {
+      id: "dailyPokemon_en",
+      img: imgPaths.dailyPokemon,
+      name: "Daily Pokémon",
+      links: links.dailyPokemonLinks,
+      short_description:
+        "Discover who's the daily Pokémon and who's your birthday Pokémon.",
     },
   ],
   "pt-br": [
     {
       id: "calculaeco_pt",
-      img: calculaecoImgPath,
+      img: imgPaths.calculaeco,
       name: "CalculaECO",
-      links: calculaecoLinks,
+      links: links.calculaecoLinks,
       short_description:
         "Ferramenta de cálculo de C.R para a Escola de Comunicação da UFRJ.",
     },
     {
       id: "bandejaobot_pt",
-      img: bandejaobotImgPath,
+      img: imgPaths.bandejaoBot,
       name: "BandejãoBot UFRJ",
-      links: bandejaobotLinks,
+      links: links.bandejaobotLinks,
       short_description:
         "Bot de Twitter que publica o cardápio dos RUs da UFRJ todos os dias às 9h.",
+    },
+    {
+      id: "dailyPokemon_en",
+      img: imgPaths.dailyPokemon,
+      name: "Daily Pokémon",
+      links: links.dailyPokemonLinks,
+      short_description:
+        "Descubra o Pokémon diário e o Pokémon do seu aniversário.",
     },
   ],
 }
