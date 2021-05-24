0ff586964a5a092d79449b10435b489b21194751
liaporto <moonkoala23@gmail.com>
2021-05-24 11:41:18 -0300

    Change font-size on large screens and update experience text

--- site/_data/site_data/experience.js
+++ site/_data/site_data/experience.js
@@ -38,9 +38,10 @@ module.exports = {
       title: "Estagiária de Experiência do cliente",
       place: "Naturgy Brasil",
       description:
-        "- Design e implementação do menu de navegação do portal de auto atendimento Minha Naturgy;\n- Atualizações de layout e criação de funcionalidades para a central de ajuda interna e para o Minha Naturgy;\n- Redação de texto e mudanças de layout visando melhorias de SEO e usabilidade;\n- Análise de métricas de uso com Google Analytics e Google Data Studio;\n- Desenvolvimento, aplicação e análise de resultados de pesquisas de satisfação.",
+        "- Design e implementação de menu de navegação para o portal Minha Naturgy;\n- Atualizações de layout e criação de funcionalidades para as centrais de ajuda internas e externas;\n- Redação de texto e mudanças de layout visando melhorias de SEO e usabilidade;\n- Análise de métricas de uso com Google Analytics e Google Data Studio;\n- Desenvolvimento, aplicação e análise de resultados de pesquisas de satisfação.",
       period: "Jan 2020 - Atualmente",
       media: [
+
         {
           link: "https://www.minhanaturgy.com.br",
           title: "Minha Naturgy",
