8b7138c357065e06bfd39e365b72cb3bae101b72
Tai Porto <moonkoala23@gmail.com>
2023-05-29 14:37:29 -0300

    Update personalData.js

--- site/_data/personalData.js
+++ site/_data/personalData.js
@@ -2,5 +2,5 @@ module.exports = {
   firstName: "Tai",
   lastName: "Porto D. Barcellos",
   email: "tai.dporto@gmail.com",
-  location: "Brasil, Rio de Janeiro - RJ",
+  location: "Brasil, Niterói - RJ",
 };
